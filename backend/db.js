7d88f34c4e7f9312217a7b14eacf1082d5b2e153
shahid321fw11 <sidansari20@gmail.com>
2026-06-10 18:32:34 +0530

    some changes in the backend db

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,11 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-// Create a connection pool
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  // Cloud providers automatically set NODE_ENV to 'production'. 
+  // This turns on SSL in the cloud, but keeps it off for your local machine.
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
